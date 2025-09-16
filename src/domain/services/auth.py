c41cec5144e6e24c4ad92e5bdb74688a22f18056
RomanTuras <salden.com@gmail.com>
2025-09-16 20:45:34 +0300

    Refactor code

--- src/domain/services/auth.py
+++ src/domain/services/auth.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
 
 from src.core.config import get_settings
-from src.dependencies.neon_db import get_session as get_pg_session
+from src.dependencies.neon_db import get_session
 from src.domain.schemas.neon.user import UserResponse
 from src.domain.services.user_service import UserService
 
@@ -42,7 +42,7 @@ async def create_access_token(data: dict, expires_delta: Optional[int] = None):
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_pg_session)
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_session)
 ) -> UserResponse:
     """Getting a current user by access token"""
     credentials_exception = HTTPException(
