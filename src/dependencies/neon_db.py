8aa15bb8e6374b0fa9bef52a8b6cbf9472b1b6f8
RomanTuras <salden.com@gmail.com>
2025-09-17 10:56:55 +0300

    Refactor code

--- src/dependencies/neon_db.py
+++ src/dependencies/neon_db.py
@@ -18,7 +18,7 @@ logger.info(
 )
 
 if settings.IS_LOCAL_MODE is False:
-    connection_string = f"{connection_string}?ssl=require&channel_binding=require"
+    connection_string = f"{connection_string}?ssl=require"
 
 async_engine = create_async_engine(connection_string, pool_recycle=300, echo=True)
 
